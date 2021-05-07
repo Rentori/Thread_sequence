a42de75fc418ac6ba0098a263b7b6406ca605ebc
Rentorry <vitya.erofeev.90@gmail.com>
2021-05-07 20:23:19 +0200

    first commit

--- src/com/company/Foo.java
+++ src/com/company/Foo.java
@@ -44,21 +44,3 @@ public class Foo {
     }
 }
 
-class FooThread implements Runnable {
-    String name;
-    Foo foo;
-
-    FooThread(Foo foo, String name) {
-        this.foo = foo;
-        this.name = name;
-    }
-
-    @Override
-    public void run() {
-        switch (name) {
-            case "A" -> foo.first();
-            case "B" -> foo.second();
-            case "C" -> foo.third();
-        }
-    }
-}
--- src/com/company/FooThread.java
+++ src/com/company/FooThread.java
@@ -1,2 +1,20 @@
-package com.company;public class FooThread {
+package com.company;
+
+class FooThread implements Runnable {
+    String name;
+    Foo foo;
+
+    FooThread(Foo foo, String name) {
+        this.foo = foo;
+        this.name = name;
+    }
+
+    @Override
+    public void run() {
+        switch (name) {
+            case "A" -> foo.first();
+            case "B" -> foo.second();
+            case "C" -> foo.third();
+        }
+    }
 }
